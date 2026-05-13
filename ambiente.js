5818d19249396ee6451cf343e83199eaacb3cc42
Pedro Xavier <arclimatizadot@gmail.com>
2026-05-13 19:34:09 -0300

    While e do/while
    Exercício da aula #16 do curso JavaScript - Curso em Vídeo.

---
+++ ambiente.js
@@ -0,0 +1,24 @@
+var c = 1
+do {
+    console.log(`Hello, World! (${c})`)
+    c++
+} while (c <= 7)
+
+
+/* var c = 1
+
+while (c <= 7) {
+    console.log(`Hello, World! (${c})`)
+    c++ // c = c + 1
+}
+*/
+
+/*
+console.log('Hello, World!')
+console.log('Hello, World!')
+console.log('Hello, World!')
+console.log('Hello, World!')
+console.log('Hello, World!')
+console.log('Hello, World!')
+console.log('Hello, World!')
+*/
