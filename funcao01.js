eac0bf0e8ad2028e866f16ca7aa3fe04a3c5d1e1
Pedro Xavier <arclimatizadot@gmail.com>
2026-07-08 20:25:08 -0300

    Fundamentos de função
    Exercício da aula #22 de JavaScript - Curso em Vídeo

---
+++ funcao01.js
@@ -0,0 +1,12 @@
+function parimpar(n) {
+    if (n%2 == 0) {
+        return 'Par'
+    } else {
+        return 'Ímpar'
+    }
+}
+
+// let res = parimpar(11)
+// console.log(res)
+
+console.log(parimpar(223))
